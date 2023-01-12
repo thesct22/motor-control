e26d8413ff6bca24176d2279b869d6693988f539
Sharath Cherian Thomas <sharathct22@gmail.com>
2023-01-12 12:23:52 +0000

    pre-commit working

--- flask-app/src/motor_control/flask/app.py
+++ flask-app/src/motor_control/flask/app.py
@@ -1,30 +1,34 @@
 # simple hello world flask app
 
 from flask import Flask
-
 from motor_control.rpi import setAngle
 
 app = Flask(__name__)
 
-@app.route('/')
-def hello_world():
-    return 'Hello World!'
 
-@app.route('/setBaseAngle/<int:angle>')
+@app.route("/")
+def hello_world():
+    return "Hello World!"
+
+
+@app.route("/setBaseAngle/<int:angle>")
 def setBaseAngle(angle):
     setAngle.setBaseAngle(angle)
-    return 'Base angle set to {}'.format(angle)
+    return "Base angle set to {}".format(angle)
 
-@app.route('/setShoulderAngle/<int:angle>')
+
+@app.route("/setShoulderAngle/<int:angle>")
 def setShoulderAngle(angle):
     setAngle.setShoulderAngle(angle)
-    return 'Shoulder angle set to {}'.format(angle)
+    return "Shoulder angle set to {}".format(angle)
 
-@app.route('/setArmAngle/<int:angle>')
+
+@app.route("/setArmAngle/<int:angle>")
 def setArmAngle(angle):
     setAngle.setArmAngle(angle)
-    return 'Arm angle set to {}'.format(angle)
+    return "Arm angle set to {}".format(angle)
+
 
 # run the app
-if __name__ == '__main__':
+if __name__ == "__main__":
     app.run()
--- flask-app/src/motor_control/rpi/setAngle.py
+++ flask-app/src/motor_control/rpi/setAngle.py
@@ -1,30 +1,32 @@
-import Rpi.GPIO as GPIO
 from time import sleep
+
+import Rpi.GPIO as GPIO
 
 GPIO.setmode(GPIO.BOARD)
 GPIO.setwarnings(False)
 
 # set up GPIO pins for 4 servo motors
 
-basePin= 03
-shoulderPin1= 05
-shoulderPin2= 07
-armPin= 11
+basePin = 3
+shoulderPin1 = 5
+shoulderPin2 = 7
+armPin = 11
 
 GPIO.setup(basePin, GPIO.OUT)
 GPIO.setup(shoulderPin1, GPIO.OUT)
 GPIO.setup(shoulderPin2, GPIO.OUT)
 GPIO.setup(armPin, GPIO.OUT)
 
-basePWM= GPIO.PWM(basePin, 50)
-shoulderPWM1= GPIO.PWM(shoulderPin1, 50)
-shoulderPWM2= GPIO.PWM(shoulderPin2, 50)
-armPWM= GPIO.PWM(armPin, 50)
+basePWM = GPIO.PWM(basePin, 50)
+shoulderPWM1 = GPIO.PWM(shoulderPin1, 50)
+shoulderPWM2 = GPIO.PWM(shoulderPin2, 50)
+armPWM = GPIO.PWM(armPin, 50)
 
 basePWM.start(0)
 shoulderPWM1.start(0)
 shoulderPWM2.start(0)
 armPWM.start(0)
+
 
 def setBaseAngle(angle):
     duty = angle / 18 + 2
@@ -33,6 +35,7 @@ def setBaseAngle(angle):
     sleep(1)
     GPIO.output(basePin, False)
     basePWM.ChangeDutyCycle(0)
+
 
 def setShoulderAngle(angle):
     duty = angle / 18 + 2
@@ -45,6 +48,7 @@ def setShoulderAngle(angle):
     GPIO.output(shoulderPin2, False)
     shoulderPWM1.ChangeDutyCycle(0)
     shoulderPWM2.ChangeDutyCycle(0)
+
 
 def setArmAngle(angle):
     duty = angle / 18 + 2
